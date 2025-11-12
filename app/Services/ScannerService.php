43d578557a3f5ec0b1d8404f37f325b23eff30e9
Gonzalo Espasandin <gonzalo.espasandin@davinci.edu.ar>
2025-11-12 12:20:45 -0300

    include ingredients relation when fetching product by barcode

--- app/Services/ScannerService.php
+++ app/Services/ScannerService.php
@@ -8,6 +8,6 @@ class ScannerService
 {
     public function process_code(string $barcode)
     {
-        return Product::where('barcode', $barcode)->first();
+        return Product::with('ingredients')->where('barcode', $barcode)->first();
     }
 }

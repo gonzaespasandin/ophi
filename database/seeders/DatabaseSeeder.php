4fdf79d66968e87e1e80bb78475caa24668e8233
Gustavo Gamero <g.gustavogamero@gmail.com>
2026-02-22 16:20:25 -0300

    Seeders para los productos, ingredientes, categorías, marcas, ingredientes de productos ahora vienen del csv en seeders/data. Normalización de toda la información y DB completa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,13 +20,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            ImportProductsFromCsvSeeder::class,
             UserSeeder::class,
-            IngredientSeeder::class,
-            CategorySeeder::class,
-            BrandSeeder::class,
-            ProductSeeder::class,
+            // IngredientSeeder::class, (ahhora vienen del ImportProductsFromCsvSeeder)
+            // CategorySeeder::class, (ahhora vienen del ImportProductsFromCsvSeeder)
+            // BrandSeeder::class, (ahhora vienen del ImportProductsFromCsvSeeder)
+            // ProductSeeder::class, (ahhora vienen del ImportProductsFromCsvSeeder)
             ProfileSeeder::class,
-            IngredientProductSeeder::class,
+            // IngredientProductSeeder::class,  (ahhora vienen del ImportProductsFromCsvSeeder)
             IngredientProfileSeeder::class,
             IngredientHasIngredientSeeder::class,
             PlanSeeder::class,

e50798f4b3e899ec1bbdcd1f491f5dda11d91753
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2026-02-28 14:39:54 -0300

    Favicon

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="{{ url('/favicon.svg') }}" >
+    <link rel="icon" href="{{ url('/favicon.ico') }}" >
     <title>{{ $title ?? 'Panel de Administración' }} | ophi</title>
 
     {{-- STYLES --}}
